a3475e0e30a5f40bfc2373971451956c796b54ac
songjiyang <352553647@qq.com>
2020-01-20 15:06:52 +0800

    内网压测

--- ab-stress-test.sh
+++ ab-stress-test.sh
@@ -1,5 +1,5 @@
 now=`date +%Y%m%d%H%M%S`
-c=5000
+c=1000
 n=10000
 warmC=10
 warmN=100
@@ -7,24 +7,24 @@ dir=stress_record/$c_$n_$now
 mkdir -p $dir
 echo "###########################warm up"
 
-ab -c $warmC -n $warmN ec2-52-82-10-77.cn-northwest-1.compute.amazonaws.com.cn/node/api/v1/hello
-ab -c $warmC -n $warmN ec2-52-82-10-77.cn-northwest-1.compute.amazonaws.com.cn/node/api/v1/compute
-ab -c $warmC -n $warmN ec2-52-82-10-77.cn-northwest-1.compute.amazonaws.com.cn/node/api/v1/listQuestionaire
+ab -c $warmC -n $warmN ip-172-31-26-49.cn-northwest-1.compute.internal/node/api/v1/hello
+ab -c $warmC -n $warmN ip-172-31-26-49.cn-northwest-1.compute.internal/node/api/v1/compute
+ab -c $warmC -n $warmN ip-172-31-26-49.cn-northwest-1.compute.internal/node/api/v1/listQuestionaire
 
-ab -c $warmC -n $warmN ec2-52-82-10-77.cn-northwest-1.compute.amazonaws.com.cn/java/api/v1/hello
-ab -c $warmC -n $warmN ec2-52-82-10-77.cn-northwest-1.compute.amazonaws.com.cn/java/api/v1/compute
-ab -c $warmC -n $warmN ec2-52-82-10-77.cn-northwest-1.compute.amazonaws.com.cn/java/api/v1/listQuestionaire
+ab -c $warmC -n $warmN ip-172-31-26-49.cn-northwest-1.compute.internal/java/api/v1/hello
+ab -c $warmC -n $warmN ip-172-31-26-49.cn-northwest-1.compute.internal/java/api/v1/compute
+ab -c $warmC -n $warmN ip-172-31-26-49.cn-northwest-1.compute.internal/java/api/v1/listQuestionaire
 
 
 
 
 echo "###########################stress test"
-ab -c $c -n $n ec2-52-82-10-77.cn-northwest-1.compute.amazonaws.com.cn/node/api/v1/hello > hello_node.txt
-ab -c $c -n $n ec2-52-82-10-77.cn-northwest-1.compute.amazonaws.com.cn/java/api/v1/hello > hello_java.txt
-ab -c $c -n $n ec2-52-82-10-77.cn-northwest-1.compute.amazonaws.com.cn/node/api/v1/compute > compute_node.txt
-ab -c $c -n $n ec2-52-82-10-77.cn-northwest-1.compute.amazonaws.com.cn/java/api/v1/compute > compute_java.txt
-ab -c $c -n $n ec2-52-82-10-77.cn-northwest-1.compute.amazonaws.com.cn/node/api/v1/listQuestionaire > listQuestionaire_node.txt
-ab -c $c -n $n ec2-52-82-10-77.cn-northwest-1.compute.amazonaws.com.cn/java/api/v1/listQuestionaire > listQuestionaire_java.txt
+ab -c $c -n $n ip-172-31-26-49.cn-northwest-1.compute.internal/node/api/v1/hello > hello_node.txt
+ab -c $c -n $n ip-172-31-26-49.cn-northwest-1.compute.internal/java/api/v1/hello > hello_java.txt
+ab -c $c -n $n ip-172-31-26-49.cn-northwest-1.compute.internal/node/api/v1/compute > compute_node.txt
+ab -c $c -n $n ip-172-31-26-49.cn-northwest-1.compute.internal/java/api/v1/compute > compute_java.txt
+ab -c $c -n $n ip-172-31-26-49.cn-northwest-1.compute.internal/node/api/v1/listQuestionaire > listQuestionaire_node.txt
+ab -c $c -n $n ip-172-31-26-49.cn-northwest-1.compute.internal/java/api/v1/listQuestionaire > listQuestionaire_java.txt
 
 grep -r  "Requests per second" *.txt
 mv *.txt $dir
