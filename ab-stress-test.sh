622807282709de031d2eb9be32ca83a21b4933ea
songjiyang <352553647@qq.com>
2020-01-20 17:43:29 +0800

    修改压测域名

--- ab-stress-test.sh
+++ ab-stress-test.sh
@@ -7,24 +7,24 @@ dir=stress_record/$c_$n_$now
 mkdir -p $dir
 echo "###########################warm up"
 
-ab -c $warmC -n $warmN ip-172-31-26-49.cn-northwest-1.compute.internal/node/api/v1/hello
-ab -c $warmC -n $warmN ip-172-31-26-49.cn-northwest-1.compute.internal/node/api/v1/compute
-ab -c $warmC -n $warmN ip-172-31-26-49.cn-northwest-1.compute.internal/node/api/v1/listQuestionaire
+ab -c $warmC -n $warmN stress.com/node/api/v1/hello
+ab -c $warmC -n $warmN stress.com/node/api/v1/compute
+ab -c $warmC -n $warmN stress.com/node/api/v1/listQuestionaire
 
-ab -c $warmC -n $warmN ip-172-31-26-49.cn-northwest-1.compute.internal/java/api/v1/hello
-ab -c $warmC -n $warmN ip-172-31-26-49.cn-northwest-1.compute.internal/java/api/v1/compute
-ab -c $warmC -n $warmN ip-172-31-26-49.cn-northwest-1.compute.internal/java/api/v1/listQuestionaire
+ab -c $warmC -n $warmN stress.com/java/api/v1/hello
+ab -c $warmC -n $warmN stress.com/java/api/v1/compute
+ab -c $warmC -n $warmN stress.com/java/api/v1/listQuestionaire
 
 
 
 
 echo "###########################stress test"
-ab -c $c -n $n ip-172-31-26-49.cn-northwest-1.compute.internal/node/api/v1/hello > hello_node.txt
-ab -c $c -n $n ip-172-31-26-49.cn-northwest-1.compute.internal/java/api/v1/hello > hello_java.txt
-ab -c $c -n $n ip-172-31-26-49.cn-northwest-1.compute.internal/node/api/v1/compute > compute_node.txt
-ab -c $c -n $n ip-172-31-26-49.cn-northwest-1.compute.internal/java/api/v1/compute > compute_java.txt
-ab -c $c -n $n ip-172-31-26-49.cn-northwest-1.compute.internal/node/api/v1/listQuestionaire > listQuestionaire_node.txt
-ab -c $c -n $n ip-172-31-26-49.cn-northwest-1.compute.internal/java/api/v1/listQuestionaire > listQuestionaire_java.txt
+ab -c $c -n $n stress.com/node/api/v1/hello > hello_node.txt
+ab -c $c -n $n stress.com/java/api/v1/hello > hello_java.txt
+ab -c $c -n $n stress.com/node/api/v1/compute > compute_node.txt
+ab -c $c -n $n stress.com/java/api/v1/compute > compute_java.txt
+ab -c $c -n $n stress.com/node/api/v1/listQuestionaire > listQuestionaire_node.txt
+ab -c $c -n $n stress.com/java/api/v1/listQuestionaire > listQuestionaire_java.txt
 
 grep -r  "Requests per second" *.txt
 mv *.txt $dir
